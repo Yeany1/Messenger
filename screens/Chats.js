20f1fc2159d5d1d514cfbaa08c3908f0a24e6268
Yeany <dinhbaoyeny2002@gmail.com>
2023-11-24 23:51:13 +0700

    done model

--- screens/Chats.js
+++ screens/Chats.js
@@ -1,10 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { useNavigation } from "@react-navigation/native";
 import UserChat from "./UserChat";
-import {StyleSheet, Text, View, Image, TouchableOpacity, TextInput, SafeAreaView, ImageBackground,} from "react-native";
 import { useEffect, useState } from "react";
 import { FlatList, ScrollView } from "react-native";
-// import { useNavigation } from "@react-navigation/native";
+import Modal from 'react-native-modal';
+
+// import { StatusBar } from "expo-status-bar";
+import {StyleSheet,Text,View,Image,TouchableOpacity,TextInput,SafeAreaView,ImageBackground, Switch} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/native-stack";
+import React from 'react'
+import { Pressable } from 'react-native';
 
 function truncateText(text, limit) {
   if (text.length > limit) {
@@ -15,6 +21,7 @@ function truncateText(text, limit) {
 }
 
 export default function Chats({ navigation }) {
+  
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -36,17 +43,116 @@ export default function Chats({ navigation }) {
         // handle error
       });
   }, []);
+  const [isModalVisible, setModalVisible] = useState(false);
 
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+  const [userData, setUserData] = useState([]);
+  const [isPressed, setIsPressed] = useState(false);
+
+  const handlePress0 = () => {
+    const navigation = useNavigation();
+    setIsPressed(true);
+  };
+  useEffect(() => {
+    fetch("https://6551dcb15c69a77903292d79.mockapi.io/User", {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        // handle error
+      })
+      .then((data) => {
+        setUserData(data);
+        // Do something with the list of tasks
+      })
+      .catch((error) => {
+        // handle error
+      });
+  }, []);
+
+
+
+  
   return (
     <SafeAreaView style={styles.container}>
       <SafeAreaView style={styles.containerHeader}>
         <SafeAreaView style={styles.group1}>
-          <TouchableOpacity style={styles.menu_icon}>
-            <Image
-              style={{ width: 40, height: 40 }}
-              source={require("/assets/icons/Menu.svg")}
-            />
+          <TouchableOpacity style={styles.menu_icon} onPress={toggleModal}>
+            <Image style={{ width: 40, height: 40 }} source={require("/assets/icons/Menu.svg")}/>
           </TouchableOpacity>
+
+          <Modal isVisible={isModalVisible} style={{ justifyContent: 'flex-end', margin: 0, height: '66%', width: '90%' }} 
+          animationIn={'slideInLeft'} animationOut={'slideOutLeft'}>
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+          {Array.isArray(userData) && userData.length > 0 && (
+          <SafeAreaView style={styles.muser}>
+            <TouchableOpacity style={styles.mbuttonUser}>
+              <Image style={{ width: 40, height: 40, borderRadius: "50%" }} source={{ uri: userData[0].avatar }}></Image>
+              <Text style={styles.mtextName}>{userData[0].firstName + " " + userData[0].lastName}</Text>
+              <Image style={{ width: 30, height: 30, marginTop: 8}} source={require('/assets/model.png')}></Image>
+              <TouchableOpacity >
+                <Image style={{ width: 30, height: 30, marginTop: 8 , marginLeft: 40}} source={require('/assets/setting.png')}></Image>
+                <Modal isVisible={isModalVisible} style={{ justifyContent: 'flex-end', margin: 0, height: '66%', width: '90%' }} 
+                  animationIn={'slideInLeft'} animationOut={'slideOutLeft'}>
+
+                  </Modal>
+
+
+
+
+
+
+
+
+              </TouchableOpacity>
+              
+            </TouchableOpacity>
+          </SafeAreaView>
+        )}
+        <View style={styles.mbody}>
+            <View style={styles.mbody1}>
+              <Pressable style={styles.mbody1} onPress={toggleModal}>
+                <Image style={styles.micon} source={require('/assets/maket3.png')}></Image>
+                <Text style={styles.mtxt}>Đoạn chat</Text>
+              </Pressable>
+            </View>
+
+            <View style={styles.mbody1}>
+              <Pressable style={styles.mbody1}>
+                <Image style={styles.micon} source={require('/assets/maket.png')}></Image>
+                <Text style={styles.mtxt}>Maketplace</Text>
+              </Pressable>
+            </View>
+
+            <View style={styles.mbody1}>
+              <Pressable style={styles.mbody1}>
+                <Image style={styles.micon} source={require('/assets/maket1.png')}></Image>
+                <Text style={styles.mtxt}>Tin nhắn đang chờ</Text>
+              </Pressable>
+            </View>
+
+            <View style={styles.mbody1}>
+              <Pressable style={styles.mbody1}>
+                <Image style={styles.micon} source={require('/assets/maket2.png')}></Image>
+                <Text style={styles.mtxt}>Kho lưu trữ</Text>
+              </Pressable>
+            </View>
+            <View style={styles.mbtntxt1}><Text style={styles.mtxt1}>Cộng đồng</Text></View>
+        </View>
+
+
+          <TouchableOpacity style={styles.mbtntxt2} onPress={toggleModal}>
+            <Text style={styles.mtxt2}>Hủy</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+
+          
           <Text style={styles.headerTitle}>Chats</Text>
         </SafeAreaView>
         <TouchableOpacity style={styles.newchat_icon}
@@ -159,6 +265,9 @@ export default function Chats({ navigation }) {
           )}
         />
       </ScrollView>
+
+
+      
     </SafeAreaView>
   );
 }
@@ -277,5 +386,78 @@ const styles = StyleSheet.create({
 
   groupChat: {
     flexDirection: "row",
+  },
+
+  muser: {
+    // alignItems: "center",
+    marginTop: 50,
+  },
+  mbuttonUser: {
+    width: '90%',
+    height: 115,
+    borderRadius: 50,
+    backgroundColor: "#fff",
+    position: "absolute",
+    top: -40,
+    left: 10,
+    flexDirection: "row",
+    
+  },
+  mtextName: {
+    fontSize: 16,
+    fontWeight: "600",
+    marginTop: 10,
+    marginLeft: 10,
+    width: "90%",
+  },
+  mbody: {
+    // flex: 1,
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  mbody1: {
+    // flex: 1,
+    marginLeft: 3,
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+    flexDirection: "row",
+    marginTop: 5,
+  },
+  mbtntxt1: {
+    position: 'relative',
+    width: "100%",
+    left: 10,
+    marginTop: 10,
+  },
+  mbtntxt2: {
+    position: 'relative',
+    width: "100%",
+    left: 300,
+    top : -25,
+    // marginTop: 10,
+  },
+  mtxt2: {
+    color: "#1395fc",
+    fontWeight: 600,
+    fontSize: 18,
+},
+  mtxt1: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#A6A6A6",
+    width: "90%",
+    textAlign: "left",
+  },
+  micon: {
+    width: 60,
+    height: 60,
+  },
+  mtxt: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginTop: 10,
+    marginLeft: 15,
   },
 });
